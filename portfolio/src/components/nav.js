2d19573ec6df04fe8b69e20538f2e1762545460e
kirk <kirk.betzina@gmail.com>
2021-08-21 16:51:24 -0400

    trying to be cool with props on cards.

--- portfolio/src/components/nav.js
+++ portfolio/src/components/nav.js
@@ -4,6 +4,9 @@ const Nav = (props) => {
     return (
         <div className='nav'>
             <h1>Nav</h1>
+            <nav className="navbar navbar-light">
+            {/* <!-- Navbar content --> */}
+            </nav>
         </div>
     )
 }
--- portfolio/src/pages/projects.js
+++ portfolio/src/pages/projects.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const Projects = (props) => {
+    const card = (props) => {
+        const numArr =[...props] 
+    }
     return(
         <div className='projects'>
             <h1>this is the projects page. All the deployed projects and Github repos will be clickable cards that will take you to the respective pages. also have an image attached.</h1>
