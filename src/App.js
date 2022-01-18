10da331efb84e3c66daeb4b956f205df7144a757
kirk <kirk.betzina@gmail.com>
2022-01-18 18:58:57 -0500

    doing basic card setups

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import Card from './components/Card';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Card />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,10 @@ import React from 'react'
 const Card = () => {
     return (
         <div className='Card'>
-            
+            <p className='title'></p>
+            <p className='github'></p>
+            <p className='website'></p>
         </div>
     )
 }
+export default Card
