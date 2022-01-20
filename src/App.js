90a6bf3dbfd275c4c74b6f7278df561b13d871a3
kirk <kirk.betzina@gmail.com>
2022-01-20 16:14:49 -0500

    getting the css right

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import aboutMe from './pages/aboutMe';
+
 
 function App() {
   return (
@@ -11,23 +11,23 @@ function App() {
             <div className='Card1'>
                 <img src='' className='car-image-top' alt=''/>
                 <div className='card-body'></div>
-                <h5 className='card-title'>Card title</h5>
-                <p className='card-text'>This is a brief description of the project</p>
-                <a href='#' className='btn btn-danger'>Go somehwere</a>
+                <h5 className='card-title'>Calorie and Macros Calculator</h5>
+                <p className='card-text'>This app will literally change your life by making the most difficlut part of diet and fitness as easy as using the app.</p>
+                <a target="_blank" rel="noopener noreferrer" href='https://github.com/KirkBetzina/Calorie-and-macro-calculation' className='btn btn-danger1'>github</a>
             </div>
             <div className='Card2'>
                 <img src='' className='car-image-top' alt=''/>
                 <div className='card-body'></div>
-                <h5 className='card-title'>Card title</h5>
-                <p className='card-text'>This is a brief description of the project</p>
-                <a href='#' className='btn btn-danger'>Go somehwere</a>
+                <h5 className='card-title'>Paper Crypto Trading App</h5>
+                <p className='card-text'>Learn to trade in the crypto markets with our free app. Prices are live! </p>
+                <a target="_blank" rel="noopener noreferrer" href='https://github.com/KirkBetzina/cryptoApp_frontend' className='btn btn-danger2'>github</a>
             </div>
             <div className='Card3'>
                 <img src='' className='car-image-top' alt=''/>
                 <div className='card-body'></div>
-                <h5 className='card-title'>Card title</h5>
-                <p className='card-text'>This is a brief description of the project</p>
-                <a href='#' className='btn btn-danger'>Go somehwere</a>
+                <h5 className='card-title'>Mintbean.io Hackathon submission</h5>
+                <p className='card-text'>My first Hackathon with Mintbean.io this project was rated as a top 16% of all the entries. </p>
+                <a target="_blank" rel="noopener noreferrer" href='https://github.com/KirkBetzina/mintbeancardgame1' className='btn btn-danger3'>github</a>
             </div>
         </>
         
