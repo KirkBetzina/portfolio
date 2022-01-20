bc2342d19eef93f9772683fc444eeb94e5936c14
kirk <kirk.betzina@gmail.com>
2022-01-20 11:40:53 -0500

    making the necessary adjustments, and found my boxes in CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import Card from './components/Card';
+
 import './App.css';
+import aboutMe from './pages/aboutMe';
 
 function App() {
   return (
     <div className="App">
-      <Card />
+      
+      <>
+            <div className='Card1'>
+                <img src='' className='car-image-top' alt=''/>
+                <div className='card-body'></div>
+                <h5 className='card-title'>Card title</h5>
+                <p className='card-text'>This is a brief description of the project</p>
+                <a href='#' className='btn btn-danger'>Go somehwere</a>
+            </div>
+            <div className='Card2'>
+                <img src='' className='car-image-top' alt=''/>
+                <div className='card-body'></div>
+                <h5 className='card-title'>Card title</h5>
+                <p className='card-text'>This is a brief description of the project</p>
+                <a href='#' className='btn btn-danger'>Go somehwere</a>
+            </div>
+            <div className='Card3'>
+                <img src='' className='car-image-top' alt=''/>
+                <div className='card-body'></div>
+                <h5 className='card-title'>Card title</h5>
+                <p className='card-text'>This is a brief description of the project</p>
+                <a href='#' className='btn btn-danger'>Go somehwere</a>
+            </div>
+        </>
+        
     </div>
+    
   );
 }
 
---
+++ src/pages/aboutMe.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const aboutMe = () =>{
+    return(
+        <div className="aboutMe">
+            <h1 className="header">Welcome</h1>
+            <p>Hello and welcome to my Portfolio website! Take a scroll down to see what I am working on!</p>
+        </div>
+    )
+}
+export default aboutMe
