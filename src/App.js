abc3347484b768715abe11713a6fcf464f98a065
kirk <kirk.betzina@gmail.com>
2022-01-20 20:17:21 -0500

    deploying with netlify

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
                 <img src='' className='car-image-top' alt=''/>
                 <div className='card-body'></div>
                 <h5 className='card-title'>Calorie and Macros Calculator</h5>
-                <p className='card-text'>This app will literally change your life by making the most difficlut part of diet and fitness as easy as using the app.</p>
-                <a target="_blank" rel="noopener noreferrer" href='https://github.com/KirkBetzina/Calorie-and-macro-calculation' className='btn btn-danger1'>github</a>
+                <p className='card-text'>This app will change your life by making the most difficlut part of diet and fitness as easy as using the app. This is my Passion Project!</p>
+                <a target="_blank" rel="noopener noreferrer" href='https://github.com/KirkBetzina/Calorie-and-macro-calculation' className='btn btn-danger1'>github</a>   
             </div>
             <div className='Card2'>
                 <img src='' className='car-image-top' alt=''/>
@@ -21,6 +21,7 @@ function App() {
                 <h5 className='card-title'>Paper Crypto Trading App</h5>
                 <p className='card-text'>Learn to trade in the crypto markets with our free app. Prices are live! </p>
                 <a target="_blank" rel="noopener noreferrer" href='https://github.com/KirkBetzina/cryptoApp_frontend' className='btn btn-danger2'>github</a>
+                <a target="_blank" rel='noopener noreferrer' href='https://mystifying-villani-51e1b2.netlify.app/login' className='btn btn-danger22'>Live Site</a>
             </div>
             <div className='Card3'>
                 <img src='' className='car-image-top' alt=''/>
@@ -28,6 +29,7 @@ function App() {
                 <h5 className='card-title'>Mintbean.io Hackathon submission</h5>
                 <p className='card-text'>My first Hackathon with Mintbean.io this project was rated as a top 16% of all the entries. </p>
                 <a target="_blank" rel="noopener noreferrer" href='https://github.com/KirkBetzina/mintbeancardgame1' className='btn btn-danger3'>github</a>
+                <a target="_blank" rel='noopener noreferrer' href='https://angry-volhard-52c28f.netlify.app/' className='btn btn-danger33'>Live Site</a>
             </div>
         </>
         
